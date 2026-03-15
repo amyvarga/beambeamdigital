16b00dd0b7fe5a01817ae3875dbd5be4c785006f
Amy Varga <amyvarga@Mac.Home>
2026-03-15 17:32:52 +0000

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat, Raleway, Bodoni_Moda } from "next/font/google";
 import "./globals.css";
 import FadeInObserver from "@/components/FadeInObserver";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 const montserrat = Montserrat({
   subsets: ["latin"],
