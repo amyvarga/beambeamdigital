bfebd7f1b69457cbdf3a256f16c843396d0f9c03
Amy Varga <amyvarga@Mac.Home>
2026-03-15 17:41:02 +0000

    added vercel analytics and speedtest to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,8 @@ export default function RootLayout({
       <body>
         <FadeInObserver />
         {children}
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
