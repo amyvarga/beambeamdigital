e3bc42682c6152a1015c85b83966debf045fb7d9
Amy Varga <amyvarga@Mac.Home>
2026-03-20 20:22:39 +0000

    fixed error on contactPanel index.tsx

--- components/ContactInfo.tsx
+++ components/ContactInfo.tsx
@@ -1,4 +1,4 @@
-type ContactItem = { contact_type: string; contact_label: string };
+type ContactItem = { contact_type: string | null; contact_label: string | null };
 
 const PhoneIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
