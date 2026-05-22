c4a46e42378209d7b0227d277ad6c18429d2da6e
Amy Varga <amyvarga@Mac.Home>
2026-05-22 12:45:33 +0100

    removed duplciate aboutSection key

--- slices/index.ts
+++ slices/index.ts
@@ -4,7 +4,6 @@ import dynamic from "next/dynamic";
 
 export const components = {
   aboutSection: dynamic(() => import("./PageSection")),
-  aboutSection: dynamic(() => import("./PageSection")),
   about_section: dynamic(() => import("./AboutSection")),
   contact_panel: dynamic(() => import("./ContactPanel")),
   faq: dynamic(() => import("./Faq")),
