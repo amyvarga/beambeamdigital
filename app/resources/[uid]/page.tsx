1c01534f21753ad0b89a4866c65129fe713d98b7
Amy Varga <amyvarga@Mac.Home>
2026-06-02 16:19:25 +0100

    added article schema

--- app/resources/[uid]/page.tsx
+++ app/resources/[uid]/page.tsx
@@ -47,8 +47,30 @@ export default async function ArticlePage({ params }: Props) {
 
   const title = asText(article.data.title);
 
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Article",
+    headline: title,
+    description: article.data.meta_description ?? article.data.excerpt ?? undefined,
+    datePublished: article.data.date ?? undefined,
+    author: article.data.author
+      ? { "@type": "Person", name: article.data.author }
+      : undefined,
+    image: article.data.featured_image?.url ?? undefined,
+    url: `https://www.beambeam.co.uk/resources/${uid}`,
+    publisher: {
+      "@type": "Organization",
+      name: "Beam Beam Digital",
+      url: "https://www.beambeam.co.uk",
+    },
+  };
+
   return (
     <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
       <BreadcrumbJsonLd label={title} path={`/resources/${uid}`} />
       <article className="page-section section">
         <div className="content article-content">
